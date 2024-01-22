1b17e9a822a24b41082fb0cd9cc793012c161711
Dmytro Belymenko <dbelimenko.99@gmail.com>
2024-01-22 10:21:42 +0200

    optimization of auth controllers and little changes in refresh-token guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,10 +4,20 @@ import {SignInDto, SignUpDto} from "./auth.dto";
 import {TokensInterface} from "../interfeces/auth.interfaces";
 import {ApiResponse, ApiTags} from "@nestjs/swagger";
 import {RefreshTokenGuard} from "../guard/refresh-token.guard";
+import { Response } from 'express';
 @ApiTags('Authentication')
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
+
+    private setCookies(res: Response, accessToken: string, refreshToken: string): void {
+        // HttpOnly flag - to prevent the token from being read from JavaScript.
+        // The secure=true flag, which will cause data to be transmitted only over HTTPS.
+        // The SameSite=strict flag should be used whenever possible, which will protect against CSRF attacks. This approach should only be used if the authorization server is from the same site as the frontend of the system.
+
+        res.cookie('accessToken', accessToken, { maxAge: 15 * 60 * 1000, httpOnly: false, secure: false, sameSite: 'strict' }) // 15 min);
+        res.cookie('refreshToken', refreshToken, { maxAge: 7 * 60 * 60 * 60 * 1000, httpOnly: true, secure: false, sameSite: 'strict' }) // 7 days);
+    }
 
 
     @Post('sign-in')
@@ -17,9 +27,7 @@ export class AuthController {
     async signIn(@Res() res: Response, @Body() signInDto: SignInDto): Promise<void> {
         try {
             const tokens = await this.authService.signIn(signInDto);
-            (res as any).cookie('accessToken', tokens.accessToken, { maxAge: 1 * 60 * 60 * 60 * 1000, httpOnly: true });
-            (res as any).cookie('refreshToken', tokens.refreshToken, { maxAge: 7 * 60 * 60 * 60 * 1000, httpOnly: true });
-            // @ts-ignore
+            this.setCookies(res, tokens.accessToken, tokens.refreshToken);
             res.status(200).send({message:"successfully"})
 
         }catch (err: unknown) {
@@ -31,25 +39,38 @@ export class AuthController {
     @ApiResponse({ status: 200, description: 'User successfully signed up' })
     @ApiResponse({ status: 400, description: 'Bad Request' })
     @ApiResponse({ status: 401, description: 'Unauthorized - Invalid credentials' })
-    async signUp( @Res() res: Response, @Body() data: SignUpDto): Promise<TokensInterface> {
-        const tokens = await this.authService.signUp(data);
-        (res as any).cookie('accessToken', tokens.accessToken, {maxAge: 1*60*60*60*1000, httpOnly: true });
-        (res as any).cookie('refreshToken', tokens.refreshToken, {maxAge: 7*60*60*60*1000, httpOnly: true });
-        return  tokens
+    async signUp( @Res() res: Response, @Body() data: SignUpDto): Promise<void> {
+        try{
+            const tokens = await this.authService.signUp(data);
+            this.setCookies(res, tokens.accessToken, tokens.refreshToken);
+            res.status(200).send({message:"successfully"})
+
+        }catch (err: unknown){
+            throw new InternalServerErrorException(err);
+        }
+
 
     }
     @UseGuards(RefreshTokenGuard)
     @Post('refresh-tokens')
     @ApiResponse({ status: 200, description: 'Refresh tokens successful' })
     @ApiResponse({ status: 401, description: 'Unauthorized' })
-    async refreshTokens(@Req() request: Request): Promise<TokensInterface> {
-        const authorizationHeader = request.headers['authorization'];
+    async refreshTokens(@Res() res: Response,@Req() request: Request): Promise<void> {
+        try {
+            const authorizationHeader = request.headers['authorization'];
 
-        if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
-            throw new Error('Bearer token not found in Authorization header');
+            if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
+                throw new Error('Bearer token not found in Authorization header');
+            }
+            const refreshToken = authorizationHeader.slice('Bearer '.length);
+            const tokens = await this.authService.refreshTokens(refreshToken);
+            this.setCookies(res, tokens.accessToken, tokens.refreshToken)
+            res.status(200).send({message:"successfully"})
+
+        }   catch (err: unknown){
+            throw new InternalServerErrorException(err);
+
         }
 
-        const refreshToken = authorizationHeader.slice('Bearer '.length);
-        return await this.authService.refreshTokens(refreshToken);
     }
 }
--- src/guard/refresh-token.guard.ts
+++ src/guard/refresh-token.guard.ts
@@ -3,7 +3,16 @@ import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class RefreshTokenGuard extends AuthGuard('jwt-refresh') {
+
+    //it is necessary to get cookies from the request because cookies are protected we can not do it through JavaScript on the client.
     canActivate(context: ExecutionContext) {
+        const request = context.switchToHttp().getRequest();
+        const refreshToken = request.cookies['refreshToken'];
+
+        if (refreshToken) {
+            request.headers['authorization'] = `Bearer ${refreshToken}`;
+        }
+
         return super.canActivate(context);
     }
 }
